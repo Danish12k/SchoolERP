8fe8fa38384ad9ff52dbf3ad21e66924930abc85
“Abhishek” <abhishek.mshr1@gmail.com>
2025-09-20 16:23:58 +0530

    addcomponent

---
+++ src/app/pages/admin/smsapi/smsapi.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smsapi',
+  imports: [],
+  templateUrl: './smsapi.component.html',
+  styleUrl: './smsapi.component.scss'
+})
+export class SmsapiComponent {
+
+}
---
+++ src/app/pages/admin/statemaster/statemaster.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statemaster',
+  imports: [],
+  templateUrl: './statemaster.component.html',
+  styleUrl: './statemaster.component.scss'
+})
+export class StatemasterComponent {
+
+}
