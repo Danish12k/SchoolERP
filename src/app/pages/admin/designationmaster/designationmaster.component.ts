060a5e62b4c95c5654feadb9dbd90a3753e8c6e8
Danish Rizwan <drizwan10@gmail.com>
2025-10-03 08:57:00 +0530

    update folder

---
+++ src/app/pages/admin/designationmaster/designationmaster.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-designationmaster',
+  imports: [],
+  templateUrl: './designationmaster.component.html',
+  styleUrl: './designationmaster.component.scss'
+})
+export class DesignationmasterComponent {
+
+}
