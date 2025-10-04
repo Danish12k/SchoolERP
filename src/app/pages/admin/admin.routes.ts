0d312c8ec0acab6e1dbe8cd229ed618fa07d45ee
Danish Rizwan <drizwan10@gmail.com>
2025-10-04 20:41:37 +0530

    session implemenation

---
+++ src/app/pages/admin/admin.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { ClassmasterComponent } from './classmaster/classmaster.component';
+import { DesignationComponent } from './designation/designation.component';
+import { DesignationmasterComponent } from './designationmaster/designationmaster.component';
+import { SessionComponent } from './session/session.component';
+import { CountryComponent } from './country/country.component';
+
+
+export const routes: Routes = [
+  { path: 'country', component: CountryComponent },
+  { path: 'classmaster', component: ClassmasterComponent },
+  { path: 'designation', component: DesignationComponent },
+  { path: 'designationmaster', component: DesignationmasterComponent },
+  { path: 'session', component: SessionComponent }
+];
