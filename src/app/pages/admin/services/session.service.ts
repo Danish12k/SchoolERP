00d6e4629ab5209cf70488703f9d39ccbbacd85e
Danish Rizwan <drizwan10@gmail.com>
2025-10-04 21:15:15 +0530

    improve api endpoints

--- src/app/pages/admin/services/session.service.ts
+++ src/app/pages/admin/services/session.service.ts
@@ -1,34 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { environment } from '@env/environment';
 import { ISession, SessionResponse } from '../interfaces/isession';
 import { Observable } from 'rxjs';
+import { apiEndpoint } from '@core/constants/apiendpoint';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
   protected readonly http: HttpClient = inject(HttpClient);
-  private baseUrl = environment.baseUrl;
-  private addSessionUrl = '/Session/AddSession';
-  private updateSessionUrl = '/Session/UpdateSession';
-  private deleteSessionUrl = '/Session/DeleteSession';
-  private sessionListUrl = '/Session/SessionList';
+
+  private endpoints = apiEndpoint; 
 
   //add session
   addSession(session:ISession):Observable<SessionResponse>{
-    return this.http.post<SessionResponse>(this.baseUrl + this.addSessionUrl, session);
+    return this.http.post<SessionResponse>(this.endpoints.ADD_SESSION, session);
   }
   updateSession(session:ISession):Observable<SessionResponse>{
-    return this.http.post<SessionResponse>(this.baseUrl + this.updateSessionUrl, session);
+    return this.http.post<SessionResponse>(this.endpoints.UPDATE_SESSION, session);
   }
   deleteSession(id:number):Observable<SessionResponse>{
-    return this.http.delete<SessionResponse>(this.baseUrl + this.deleteSessionUrl + `?id=${id}`);
-  } 
+    return this.http.delete<SessionResponse>(this.endpoints.DELETE_SESSION + `?id=${id}`);
+  }
 
   //get session list
   getSessionList():Observable<SessionResponse>{
-    return this.http.get<SessionResponse>(this.baseUrl + this.sessionListUrl);
+    return this.http.get<SessionResponse>(this.endpoints.SESSION_LIST);
   }
 
 
