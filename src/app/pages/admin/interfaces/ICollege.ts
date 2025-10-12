326161f5b4b0249ce881a37bd2b49473ea145efb
Danish Rizwan <drizwan10@gmail.com>
2025-10-12 15:52:49 +0530

    added college

---
+++ src/app/pages/admin/interfaces/ICollege.ts
@@ -0,0 +1,16 @@
+export interface ICollege {
+  collegeId: number;
+  collegeAbreviation: string;
+  collegeName: string;
+  collegeAddress: string;
+  email: string;
+  phone: string;
+  fax: string;
+  webSite: string;
+  affilicationNo: string;
+  board: string;
+  collegeLogo: string;
+  schoolCode: string;
+  dioceseCode: string;
+  portalURL: string | null;
+}
