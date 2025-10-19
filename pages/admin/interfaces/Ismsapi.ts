320c7016366a1a61df7c0f54375dd84df1a4c648
Danish Rizwan <drizwan10@gmail.com>
2025-10-19 11:46:37 +0530

    added sms api provider

---
+++ pages/admin/interfaces/Ismsapi.ts
@@ -0,0 +1,9 @@
+export interface ISMSApi{
+  id: number;
+  collegeId: number;
+  api_UserName: string;
+  api_Password: string;
+  apI_SenderId: string;
+  apI_Status: number;
+  apI_Provider: string;
+}
