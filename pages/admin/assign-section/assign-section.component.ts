e56998bf155425e6c9691dee61aae4792d138feb
“Abhishek” <abhishek.mshr1@gmail.com>
2026-04-01 21:46:35 +0530

    update changes

--- pages/admin/assign-section/assign-section.component.ts
+++ pages/admin/assign-section/assign-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatCardModule, MatCardTitle } from "@angular/material/card";
 import { MaterialModule } from "../../../../../schematics/ng-add/files/module-files/app/material.module";
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -6,12 +6,12 @@ import { SessionService } from '../../../services/session.service';
 import { SectionService } from '../../../services/section.service';
 import { CollegeService } from '../../../services/college.service';
 import { ISession } from '../../../interfaces/isession';
-import { IAssignSection, ISection } from '../../../interfaces/IClassAndSection';
+import { IAssignSection } from '../../../interfaces/IClassAndSection';
 import { ICollege } from '../../../interfaces/ICollege';
 import { IClass } from '../../../interfaces/IClassmaster';
 import { MatColumnResizeCommonModule } from "@ng-matero/extensions/grid";
-import { debug } from 'console';
 import { MatDialog } from '@angular/material/dialog';
+import { SectionFilterComponent } from '../../../shared/components/section-filter/section-filter.component';
 
 @Component({
   selector: 'app-assign-section',
@@ -20,7 +20,8 @@ import { MatDialog } from '@angular/material/dialog';
     MaterialModule,
     MatCardModule,
     ReactiveFormsModule,
-    MatColumnResizeCommonModule
+    MatColumnResizeCommonModule,
+    SectionFilterComponent,
   ],
   templateUrl: './assign-section.component.html',
   styleUrl: './assign-section.component.scss'
@@ -34,31 +35,19 @@ export class AssignSectionComponent implements OnInit {
 
   assignSectionForm!: FormGroup;
   sessions!: ISession[];
-  sections!: ISection[];
   schools!: ICollege[];
   classs!: IClass[];
 
   ngOnInit(): void {
-    debugger;
     this.assignSectionForm = this.fb.group({
       sessionId: [null, [Validators.required, Validators.min(1)]],
       collegeId: [null, [Validators.required, Validators.min(1)]],
       courseId: [null, [Validators.required, Validators.min(1)]],
       sectionId: [null, [Validators.required, Validators.min(1)]],
     });
-    debugger;
     this.loadSession();
     this.loadCollege();
-    this.loadSection();
   };
-
-
-  loadSection() {
-    debugger;
-    this.sectionService.getSectionList().subscribe(res => {
-      this.sections = Array.isArray(res.data) ? res.data : [res.data];
-    });
-  }
 
   loadSession() {
     this.sessionService.getSessionList().subscribe(res => {
@@ -85,23 +74,19 @@ export class AssignSectionComponent implements OnInit {
 
     this.collegeService.getClassListBySessionAndCollege(sessionId, collegeId).subscribe({
       next: (res) => {
-        debugger;
         if (res.success && res.data) {
           this.classs = Array.isArray(res.data) ? res.data : [res.data];
         } else {
-          debugger;
           alert(res.message);
         }
       },
-      error: (err) => {
-        debugger;
+      error: () => {
         console.log("error");
       }
     });
   }
 
   submitForm() {
-    debugger;
     if (this.assignSectionForm.valid) {
       const courseId = this.assignSectionForm.get('courseId')?.value;
       const sectionId = this.assignSectionForm.get('sectionId')?.value;
--- services/student.service.ts
+++ services/student.service.ts
@@ -16,4 +16,10 @@ export class StudentService {
   listStudents(): Observable<IApiResponse<IStudent>> {
     return this.http.get<IApiResponse<IStudent>>(this.endPoints.student.list);
   }
+
+  /** Students in a section (`Active`: 1 = active, 0 = inactive). */
+  listStudentsBySection(sectionId: number, active: number = 1): Observable<IApiResponse<IStudent>> {
+    const url = `${this.endPoints.student.sectionStudentList}?SectionId=${sectionId}&Active=${active}`;
+    return this.http.get<IApiResponse<IStudent>>(url);
+  }
 }
